4612bdbc52cd6a07228eea68fa8e33598d198bc3
shakhawat hossain <94shakha.18@gmail.com>
2018-12-17 17:15:35 +0600

    initial

---
+++ database/migrations/2018_12_10_052610_create_sells_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSellsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sells', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('customer_id');
+            $table->string('branch_id')->default(1);
+            $table->double('total_amount');
+            $table->double('paid_amount')->default(0);
+            $table->string('sell_date')->nullable();
+            $table->double('disount_amount')->default(0);
+            $table->tinyInteger('payment_method')->default(0);
+            $table->tinyInteger('payment_status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sells');
+    }
+}
